55cba3ae4dafa88fde3cd86104c05a1ee79e1b22
Ian-MICTO <ianmicto@gmail.com>
2026-05-15 17:54:39 +0800

    fix: removed unused variables

--- components/docs-sidebar.tsx
+++ components/docs-sidebar.tsx
@@ -3,7 +3,6 @@
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { title } from "process";
 
 import { docsConfig } from "@/config/docs";
 import registry from "@/registry.json";
